d33b9c68d31dd5157df48529ab5502592dd8f6ec
Bolter <novohatskiyso@gmail.com>
2020-12-23 14:36:59 +0500

    OOP

--- iiBot.py
+++ iiBot.py
@@ -3,69 +3,72 @@ import json
 from fuzzywuzzy import fuzz
 
 
-def fileLoadText(path):
-    fileOpenName = path
-    with open(fileOpenName, encoding="utf-8") as fl:
-        phrases = json.load(fl)
-        return phrases
-
-
-def iiBotResponse():
-    listOfFiles = os.listdir(path="bot.say")
-    i = 0
+class conversationBot:
     responceBot = []
-    while i < len(listOfFiles):
-        x = (fileLoadText("bot.say\\" + listOfFiles[i]))
-        i += 1
-        responceBot.append(x)
-    return responceBot
-
-
-def usersBotPhrases():
-    listOfFiles = os.listdir(path="user.say")
-    i = 0
     usersSay = []
-    while i < len(listOfFiles):
-        x = fileLoadText("user.say\\" + listOfFiles[i])
-        i += 1
-        usersSay.append(x)
-    return usersSay
 
+    def __init__(self):
+        self.iiBotResponse()
+        self.usersBotPhrases()
 
-responceBot = iiBotResponse()
-usersSay = usersBotPhrases()
+    def fileLoadText(self, path):
+        fileOpenName = path
+        with open(fileOpenName, encoding="utf-8") as fl:
+            phrases = json.load(fl)
+            return phrases
 
+    def iiBotResponse(self):
+        listOfFiles = os.listdir(path="bot.say")
+        i = 0
+        while i < len(listOfFiles):
+            x = (self.fileLoadText("bot.say\\" + listOfFiles[i]))
+            i += 1
+            self.responceBot.append(x)
 
-def comparison(text):
-    text = text.lower()
-    k = 0
-    k1 = 0
-    k2 = 0
-    counter = 0
-    while counter < len(usersSay):
-        counter2 = 0
-        while counter2 < len(usersSay[counter]):
-            j = fuzz.ratio(usersSay[counter][counter2], text)
-            counter2 += 1
-            if j > k:
-                k = j
-                k1 = counter
-                k2 = counter2
-        counter += 1
-        if k > 40:
-            k3 = True
-        else: k3 = False
-    print(k)
-    print(usersSay[k1][k2 - 1])
-    return [k3, k1, k2 - 1]
+    def usersBotPhrases(self):
+        listOfFiles = os.listdir(path="user.say")
+        i = 0
+        while i < len(listOfFiles):
+            x = self.fileLoadText("user.say\\" + listOfFiles[i])
+            i += 1
+            self.usersSay.append(x)
+        return self.usersSay
+
+    def comparison(self, text):
+        text = text.lower()
+        k = 0
+        counterInterior = 0
+        counterExternal = 0
+        counter = 0
+        while counter < len(self.usersSay):
+            counter2 = 0
+            while counter2 < len(self.usersSay[counter]):
+                j = fuzz.ratio(self.usersSay[counter][counter2], text)
+                counter2 += 1
+                if j > k:
+                    k = j
+                    counterInterior = counter
+                    counterExternal = counter2
+            counter += 1
+            if k > 40:
+                result = True
+            else:
+                result = False
+        return [result, counterInterior, counterExternal - 1]
+
+    def dialog(self, text):
+        respounce = self.comparison(text)
+        if respounce[0]:
+            print("asd " + str(respounce[2]))
+            respounceFrase = self.responceBot[respounce[1]][respounce[2]]
+        else:
+            respounceFrase = "Я не смог разобраться."
+        return respounceFrase
 
 
 def main():
-    while True:
-        text = input("Введите текст: ")
-        print(comparison(text))
-
-    print(comparison("kvh4twESDG,,jhkgGHSFAFGsf"))
+    q = conversationBot()
+    print(q.dialog("привет"))
 
 
 if __name__ == "__main__":
